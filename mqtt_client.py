cc6f86f5187ba034c38bb6c3d563ed19ee72d9d3
seasons986 <annalin12345@gmail.com>
2024-02-27 19:05:16 -0500

    first mqtt try

---
+++ mqtt_client.py
@@ -0,0 +1,30 @@
+import time
+import paho.mqtt.client as mqtt
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, reason_code):
+    print(f"Connected with result code {reason_code}")
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("$SYS/#")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+mqttc = mqtt.Client("pi1")
+# mqttc.username_pw_set("theyonetwork","ConnDevSP24")
+mqttc.on_connect = on_connect
+mqttc.on_message = on_message
+
+# broker.hivemq.com
+mqttc.connect("theyonetwork.cloud.shiftr.io", 1883, 60)
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+mqttc.loop()
+
+while True:
+    mqttc.loop()
