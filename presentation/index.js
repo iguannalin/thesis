8b585c41bd27ba621bf2cec2fe363abf4d30d41f
seasons986 <annalin12345@gmail.com>
2024-04-24 14:57:44 -0400

    gotta go

--- presentation/index.js
+++ presentation/index.js
@@ -8,6 +8,7 @@ window.addEventListener("load", () => {
   let infos = [];
   let interval;
   let index = 0;
+  let paused = false;
 
   fetch("media.json").then((r)=>r.json()).then((d) => {
     infos=Array.from(d);
@@ -16,11 +17,13 @@ window.addEventListener("load", () => {
 
   function start(e) {
     e.preventDefault();
-    if (index == 0) createWindow();
+    interval = setInterval(createWindow, 6000);
+    // if (index == 0) createWindow();
   }
 
   function createWindow() {
-    let sq = getRandomInt(200,500);
+    // if (paused) return;
+    let sq = 480; //getRandomInt(200,500);
     const info = infos[index];
     const text = `<!DOCTYPE html><html> <head> <title>absence</title> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1"> <link rel="stylesheet" href="https://iguannalin.github.io/thesis/presentation/index.css"/></head> <body><div id="overlay"></div> <div id="container" data-info=${btoa(info)}></div></body><script>let container = document.getElementById('container'); if (container.dataset.info) container.innerHTML = atob(container.dataset.info);</script></html>`;
     const blob = new Blob([text], {type: "text/html"});
@@ -28,7 +31,7 @@ window.addEventListener("load", () => {
     window.open(blobUrl, '_blank', `location=0,menubar=0,status=0,scrollbars=0,toolbar=0,resizable=0,popup,width=${sq},height=${sq},left=${getRandomInt(0,screen.width)},top=${getRandomInt(0,screen.height)}`);
     window.URL.revokeObjectURL(blobUrl);
     index++;
-    if (index == 1) interval = setInterval(createWindow, 5000);
+    // if (index == 1) interval = setInterval(createWindow, 5000);
     console.log({index, infos});
     if (index >= infos.length) 
       clearInterval(interval);
