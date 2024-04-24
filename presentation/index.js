a6521779f2d0c3c14369e6bb7625420ea3c84baa
seasons986 <annalin12345@gmail.com>
2024-04-24 14:12:46 -0400

    positions

--- presentation/index.js
+++ presentation/index.js
@@ -25,7 +25,7 @@ window.addEventListener("load", () => {
     const text = `<!DOCTYPE html><html> <head> <title>absence</title> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1"> <link rel="stylesheet" href="https://iguannalin.github.io/thesis/presentation/index.css"/></head> <body><div id="overlay"></div> <div id="container" data-info=${btoa(info)}></div></body><script>let container = document.getElementById('container'); if (container.dataset.info) container.innerHTML = atob(container.dataset.info);</script></html>`;
     const blob = new Blob([text], {type: "text/html"});
     const blobUrl = URL.createObjectURL(blob);
-    window.open(blobUrl, '_blank', `location=0,menubar=0,status=0,scrollbars=0,toolbar=0,resizable=0,popup,width=${sq},height=${sq},left=${getRandomInt(300,900)},top=${getRandomInt(100,500)}`);
+    window.open(blobUrl, '_blank', `location=0,menubar=0,status=0,scrollbars=0,toolbar=0,resizable=0,popup,width=${sq},height=${sq},left=${getRandomInt(0,screen.width)},top=${getRandomInt(0,screen.height)}`);
     window.URL.revokeObjectURL(blobUrl);
     index++;
     if (index == 1) interval = setInterval(createWindow, 20);
