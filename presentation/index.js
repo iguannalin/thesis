a4c0db2b9669ac89fea5c1db6cfd2178cf55a8ed
seasons986 <annalin12345@gmail.com>
2024-05-08 15:04:11 -0400

    update bg colors

--- presentation/index.js
+++ presentation/index.js
@@ -24,13 +24,16 @@ window.addEventListener("load", () => {
   function createWindow() {
     // if (paused) return;
     let WW = 480; //getRandomInt(200,500);
-    let HH = "auto";
-    // let HH = 480; //getRandomInt(200,500);
+    let HH = 240; //getRandomInt(200,500);
+    let color = "white";
     const info = infos[index];
-    // if (!info.includes("img")) HH = 240;
+    if (info.includes("img")) {
+      HH = 480;
+      color = "rgb(35, 35, 35)";
+    }
     const text = `<!DOCTYPE html><html> <head> <title>HCI</title> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1"> 
     <link rel="stylesheet" href="https://iguannalin.github.io/thesis/presentation/index.css"/></head>
-    <body><div id="overlay"></div> <div id="container" data-info=${btoa(info)}></div></body><script>let container = document.getElementById('container'); if (container.dataset.info) container.innerHTML = atob(container.dataset.info);</script></html>`;
+    <body><div id="overlay"></div> <div id="container" data-info=${btoa(info)}></div></body><script>let container = document.getElementById('container'); if (container.dataset.info) container.innerHTML = atob(container.dataset.info); container.style.backgroundColor = ${color}</script></html>`;
     const blob = new Blob([text], {type: "text/html"});
     const blobUrl = URL.createObjectURL(blob);
     window.open(blobUrl, '_blank', `location=0,menubar=0,status=0,scrollbars=0,toolbar=0,resizable=0,popup,width=${WW},height=${HH},left=${getRandomInt(0,screen.width)},top=${getRandomInt(0,screen.height)}`);
