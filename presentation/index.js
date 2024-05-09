c761349d038c8bcdb875e812a4aa50ce5a02f8db
seasons986 <annalin12345@gmail.com>
2024-05-09 01:49:59 -0400

    got rid of lag?

--- presentation/index.js
+++ presentation/index.js
@@ -5,23 +5,21 @@ window.addEventListener("load", () => {
     return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
   }
   let infos = [];
-  let interval;
-  let index = 0;
 
   fetch("media.json").then((r)=>r.json()).then((d) => {
     infos=Array.from(d);
     document.addEventListener('click', start, {passive: false});
+    document.getElementById("begin").addEventListener("click", (el) => el.target.innerHTML = "🇵🇸");
   });
 
   function start(e) {
     e.preventDefault();
-    interval = setInterval(createWindow, 5500);
-    // interval = setInterval(createWindow, 500);
-    // interval = setInterval(createWindow, 600);
+    for (let i = 0; i < infos.length; i++) {
+      setTimeout(()=>{createWindow(i);}, (i+1) * 5500);
+    }
   }
 
-  function createWindow() {
-    // if (paused) return;
+  function createWindow(index) {
     let WW = 480; //getRandomInt(200,500);
     let HH = 240; //getRandomInt(200,500);
     let color = "white";
@@ -30,15 +28,10 @@ window.addEventListener("load", () => {
       HH = 480;
       color = "rgb(35, 35, 35)";
     }
-    const text = `<!DOCTYPE html><html> <head> <title>HCI</title> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1"> 
-    <link rel="stylesheet" href="http://127.0.0.1:5500/index.css"/></head>
-    <body><div id="overlay"></div> <div id="container" data-info=${btoa(info)}></div></body><script>let container = document.getElementById('container'); if (container.dataset.info) container.innerHTML = atob(container.dataset.info); document.body.style.backgroundColor = '${color}';</script></html>`;
+    const text = `<!DOCTYPE html><html><head> <title>HCI</title> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="http://127.0.0.1:5500/index.css"/></head><body><div id="container" data-info=${btoa(info)}></div></body><script>let container = document.getElementById('container'); if (container.dataset.info) container.innerHTML = atob(container.dataset.info); document.body.style.backgroundColor = '${color}';</script></html>`;
     const blob = new Blob([text], {type: "text/html"});
     const blobUrl = URL.createObjectURL(blob);
     window.open(blobUrl, '_blank', `location=0,menubar=0,status=0,scrollbars=0,toolbar=0,resizable=0,popup,width=${WW},height=${HH},left=${getRandomInt(0,screen.width)},top=${getRandomInt(0,screen.height-300)}`);
     window.URL.revokeObjectURL(blobUrl);
-    index++;
-    if (index >= infos.length) 
-      clearInterval(interval);
   }
 });
